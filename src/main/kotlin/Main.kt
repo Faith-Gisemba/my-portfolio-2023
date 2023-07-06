33d6ce64a213611ee814a448c940410c21f0edf0
Faith-Gisemba <faithmgisemba@gmail.com>
2023-07-06 10:05:09 +0300

    added stacks

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,54 @@
+fun main(){
+//    var myStack = Stack()
+//
+//    myStack.push(67)
+//    myStack.push(47)
+//    println(myStack.peek())
+//    myStack.pop()
+//    myStack.push(90)
+//
+//    var x = myStack.pop()
+//    var y = myStack.pop()
+//    var z = x!! + y!!
+//    println(z)
+    var myStack = Stack()
+    var string = "adalab"
+    for(c in string){
+        myStack.push(c)
+    }
+    var result = mutableListOf<Char>()
+    while (!myStack.isEmpty()){
+        result.add(myStack.pop()!!)
+    }
+    println(result.joinToString(""))
+
+}
+class Stack{
+    var data = mutableListOf<Char>()
+
+    fun push(value:Char){
+        data.add(value)
+    }
+
+    fun pop():Char?{
+        if(data.isEmpty()){
+            return null
+        }
+
+        var top = data[data.lastIndex]
+        data.removeAt(data.lastIndex)
+        return top
+    }
+
+    fun peek() :Char?{
+        if(data.isEmpty()) {
+            return null
+        }
+        return data[data.lastIndex]
+    }
+
+    fun isEmpty(): Boolean{
+        return data.isEmpty()
+    }
+
+}
